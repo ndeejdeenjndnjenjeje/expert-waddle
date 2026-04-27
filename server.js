3cedf493f59d2651e2a6d1660edc53b27cc3c2ea
ndeejdeenjndnjenjeje <wangnick332@gmail.com>
2026-04-27 00:08:56 -0700

    force railway working test

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
 
+// TEST ROUTE
 app.get("/", (req, res) => {
   res.send("🚀 Railway is working");
 });
 
+// IMPORTANT PORT FIX
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
-  console.log("Server running on " + PORT);
+  console.log("Server running on port " + PORT);
 });
